87618105d2e90ac45e61ec1d099d97b134390c8f
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-02-08 00:52:40 -0500

    added assignment 2

--- assignment2/README.md
+++ assignment2/README.md
@@ -1,11 +1,13 @@
-# My Movements
+# What does studying looks like?
 
-This map visualize all my movements being track for 13 days.
 
-My movements
+This map visualize almost all of my movements, for 13 days, during my first two weeks for the Spring 2017 term.
+It is based on data gather from [Moves](https://moves-app.com/) so it is not very accurate.
+
+###### Disclaimer: I let my phone at home a couple of days.
 
 ## Live
 
-Live Here
+[Live Here](https://cvalenzuela.github.io/Data-and-Digital-Mapping/assignment2/index.html)
 
 ###### By Cristóbal Valenzuela
---
+++ assignment2/src/js/script.js
@@ -0,0 +1,149 @@
+/*
+
+My movents in 13 days
+
+cvalenzuelab@nyu.edu
+*/
+
+
+//=====================
+// Variables
+//====================
+var animatioBtn = null, markerBtn = null, showAllBtn = null, erase = null, path = null, latlngs = null, len = route.length, animatedMarker = null, data = null;
+
+//=====================
+// When DOM ready
+//====================
+let loaded = () =>  {
+  // Get the buttons
+  animatioBtn = document.getElementById('animation');
+  markerBtn = document.getElementById('marker');
+  showAllBtn = document.getElementById('show-all');
+  erase = document.getElementById('erase');
+
+  // Listen for the elements
+  animatioBtn.addEventListener('click', animate);
+  markerBtn.addEventListener('click', addMarker);
+  showAllBtn.addEventListener('click', showAll);
+  erase.addEventListener('click', eraseAll);
+
+}
+
+//=====================
+// Base Map
+//====================
+var map = L.map('map', {
+  zoom: 15,
+  center: [ 40.736770853932505,-73.98880004882814,]
+  // zoomControl: false,
+  // trackResize: false,
+  // dragging: false,
+  // zoomAnimation: false,
+  // scrollWheelZoom: false
+
+});
+
+var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png');
+map.addLayer(baseLayer);
+
+//=====================
+// Show all Data
+//====================
+let showAll = () => {
+
+  // Clean everything
+  eraseAll();
+
+  data = new L.GeoJSON.AJAX("../../data/full/activities.geojson", {
+    color: '#FFD002',
+    opacity: 0.5,
+    weight: 6
+  }).addTo(map);
+}
+
+//=====================
+// Marker
+//====================
+
+
+let addMarker = () => {
+
+  var customIcon = L.icon({
+    iconUrl: 'src/css/icon.png',
+    iconSize: [14,14]
+  });
+
+  // Clean everything
+  eraseAll();
+
+  for (let i = 0; i < len; i++) {
+    latlngs.push(new L.LatLng(route[i][1], route[i][0]));
+  }
+
+  path = L.polyline(latlngs, {
+    color: '#FF5233',
+    opacity: 0.5
+  });
+
+  animatedMarker = L.animatedMarker(path.getLatLngs(), {
+    distance: 100 ,  // meters
+    interval: 200, // milliseconds
+    icon: customIcon
+  });
+  map.addLayer(animatedMarker);
+}
+
+//=====================
+// Animate
+//====================
+let animate = () => {
+
+  // Clean everything
+  eraseAll();
+
+  for (let i = 0; i < len; i++) {
+    latlngs.push(new L.LatLng(route[i][1], route[i][0]));
+  }
+
+  path = L.polyline(latlngs, {
+    snakingSpeed: 80,
+    snakingPause: 0,
+    color: '#FFD002',
+    opacity: 0.5,
+    weight: 6
+  });
+
+  map.fitBounds(L.latLngBounds(latlngs));
+  map.addLayer(path);
+  path.snakeIn();
+}
+
+//=====================
+// Events for paths
+//====================
+if(path != null){
+  path.on('snakeend', function(ev){
+    console.log('Done!');
+  });
+}
+
+//=====================
+// Erase All
+//====================
+
+var eraseAll = () => {
+  if(path != null){
+    map.removeLayer(path);
+  }
+  if(animatedMarker != null){
+    map.removeLayer(animatedMarker);
+  }
+  if(data != null){
+    map.removeLayer(data);
+  }
+  latlngs = [];
+  path = null;
+}
+
+// On window loaded
+window.addEventListener('load',loaded);
